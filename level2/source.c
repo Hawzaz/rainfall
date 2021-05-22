7801210e5594a540bc3e31aae807b3e1f16894e0
Hawzaz <mellonbenjamin@gmail.com>
2021-05-22 17:57:44 +0200

    begin source and walkthrough

---
+++ level2/source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void	p(void)
+{
+	char buff[76];
+	fflush(stdout);
+	gets(buff);
+	if ()
+	{
+		printf("0x804854a");
+		strdup(
+	}
+	else
+	{
+		printf();
+		exit(0);
+	}
+}
+
+int		main()
+{
+	p();
+	return (0);
+}
