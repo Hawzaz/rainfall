2df5ee0fcd99f956eed127c24e752e04fb8f51ab
Koda <mellonbenjamin@gmail.com>
2021-11-12 19:01:00 +0100

    push blank full files

--- bonus0/source.c
+++ bonus0/source.c
@@ -1,30 +1,33 @@
-int p (char *dest, char *s)
+void p (char *dest, char *s)
 {
-    char buff[64];
-    char *src;
+	char buff[4120];
+	char *src;
 
-    puts(s);
-    read(0, 4096, buff);
-    strchr(buff, 10);
-    buff[20] = 0;
-    strncpy(buff, 20, dest)
-    return (dest);
+	puts(s);
+	read(0, buff, 4096);
+	*strchr(buff, '\n') = 0;
+	
+	strncpy(buff, dest, 20);
+	return;
 }
 
-unsigned int pp(char *dest)
+void pp(char *dest)
 {
-    char *src;
-    p(src," - ");
-    p(src," - ");
-    strcpy(src, dest);
-    strcat(s1, s2);
-    return (dest);
+	char s1[20];
+	char s2[20];
+
+	p(s1," - ");
+	p(s2," - ");
+	strcpy(s1, dest);
+	dest[strlen(dest)] = ' ';
+	strcat(s2, dest);
+	return;
 }
 
 int main(void)
 {
-    char *s;
-    pp(&s);
-    puts(&s)
-    return 0;
+	char *s;
+	pp(s);
+	puts(s);
+	return 0;
 }
---
+++ level9/source.cpp
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <iostream>
+#include <cstring>
+
+int main (char **ac, char **av) {
+	
+	int var_bp_4h;
+	int var_4h;
+	int var_10h;
+	int var_14h;
+	int var_18h;
+	int var_1ch;
+	
+	if (av <= 1) {
+		_exit(1);
+	}
+
+	N *n5 = new N(5);
+	N *n6 = new N(6);
+	var_18h = ebx;
+	eax = var_1ch;
+	var_14h = var_1ch;
+	eax = var_18h;
+	var_10h = var_18h;
+	eax = envp;
+	eax += 4;
+	eax = *(eax);
+	eax = var_14h;
+	nn5.setAnnotation(argv[1]);
+	eax = var_10h;
+	eax = *(eax);
+	edx = *(eax);
+	eax = var_14h;
+	eax = var_10h;
+	void (*edx)(void, void) (eax, var_14h);
+	ebx = var_bp_4h;
+	return eax;
+}
