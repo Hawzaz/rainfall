b77ef4dc953a6f71343d75ca8dca618b8c8a9e24
Koda <mellonbenjamin@gmail.com>
2021-11-19 16:45:11 +0100

    end of source

--- bonus3/source.c
+++ bonus3/source.c
@@ -9,19 +9,16 @@ int main(int ac, char **av)
 
 	file = fopen("/home/user/end/.pass", 'r');
 	memset(str, '0', 33);
-	if (file)
+	if (file && ac == 2)
 	{
-		if (ac == 2)
-		{
-			fread(str, 66, 1, file);
-			atoi(av[1]);
-			fread(str2, 65, 1, file);
-			fclose(file);
-			if (strcmp(str, av[2] == 0))
-				execl("/bin/sh", 0, "sh");
-			else
-				puts(str + 66);
-		}
+		fread(str, 66, 1, file);
+		str[atoi(av[1])] = 0;
+		fread(str2, 65, 1, file);
+		fclose(file);
+		if (strcmp(str, av[2] == 0))
+			execl("/bin/sh", "sh", 0);
+		else
+			puts(str + 66);
 	}	
 	return 0;
 }
